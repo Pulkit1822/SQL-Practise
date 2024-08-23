7762e83a2d9685200c03f0dad5e89815ca91a39a
Pulkit Kumar Mathur <pulkitkumarmathur1@gmail.com>
2024-08-23 13:20:20 +0530

    chore: Update employee table with new data and perform updates and deletions

---
+++ UpdateQueryPractise(L12).sql
@@ -0,0 +1,33 @@
+-- Creation of database and using the same
+CREATE DATABASE IF NOT EXISTS company1;
+USE company1;
+
+-- Creation of employee table
+CREATE TABLE IF NOT EXISTS employee (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    age INTEGER NOT NULL,
+    department VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    salary INTEGER NOT NULL
+);
+
+-- Insertion of data
+INSERT INTO employee (name, age, department, city, salary) VALUES
+('John', 24, 'IT', 'New York', 50000),
+('Jane', 25, 'HR', 'London', 6000),
+('Alice', 26, 'Finance', 'Paris', 70000),
+('Pulkit', 21, 'HR', 'London', 8000);
+
+-- Viewing of database
+SELECT * FROM employee;
+
+-- Update query
+SET SQL_SAFE_UPDATES = 0;
+UPDATE employee
+SET salary = 50000
+WHERE department = 'HR';
+
+UPDATE employee
+set name = 'Pulkit Mathur'
+WHERE name = 'Pulkit';
